9adc232d0faf06b962a395e693b91f1a08933e18
Brenda Ferrufino <ferrufinob@gmail.com>
2021-03-22 16:36:01 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+// function expression also an anonymous function
+const divide = function () {
+  return 2000 / 100;
+};
+
+const square = (num) => num * num;
+
+const add = (num1, num2) => num1 + num2;
